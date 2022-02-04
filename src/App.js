885fad2253d5dfce39deb19cddf2a81310e79a6d
Paul-Semenko <paul.semenko.ps@gmail.com>
2022-02-04 21:42:00 +0200

    registration form-markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import RegistrationForm from '../src/components/RegistrationForm/RegistrationForm';
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+  <h1>Hello world!</h1>
+  
+    <RegistrationForm/>
+  </>
+  )
+  
 }
 
 export default App;
