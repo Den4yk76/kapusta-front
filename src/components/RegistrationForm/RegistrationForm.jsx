dec511bb18636ed6d6df72c1b5e289b9ad224d96
Paul-Semenko <paul.semenko.ps@gmail.com>
2022-02-05 12:19:30 +0200

    fix is done

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -5,18 +5,18 @@ export default function RegistrationForm() {
     return (
         <>
             <form className={s.ContactForm}>
-                <h4 className={s.FormTitle}>Вы можете авторизоваться с помощью Google Account:</h4>
+                <h4 className={s.FormTitle}>You are able to sign in with Google Account:</h4>
                 <a href="" className={s.FormLink}><svg width="17" height="18"><use></use></svg>Google</a>
-                <h4 className={s.FormSubTitle}>Или зайти с помощью e-mail и пароля, предварительно зарегистрировавшись:</h4>
+                <h4 className={s.FormSubTitle}> Or pass the user authentication using your account's login and password, prior to registration:</h4>
                 <div className={s.wraper}>
-                <label className={s.FormLabel} for="emailInput">Электронная почта:</label>
+                <label className={s.FormLabel} for="emailInput">Email:</label>
                     <input id="emailInput" type="email" placeholder="your@email.com" className={s.FormInput}/>                    
                 
-                <label className={s.FormLabel} for="passwordInput">Пароль:</label>
-                <input id="passwordInput" type="password" placeholder="Пароль" className={`${s.FormInput} ${s.FormInputIndent}`} />
+                <label className={s.FormLabel} for="passwordInput">Password:</label>
+                <input id="passwordInput" type="password" placeholder="Password" className={`${s.FormInput} ${s.FormInputIndent}`} />
                 <div className={s.wrap}>
-                <button type="submit" className={s.btn}>Войти</button>
-                <button type="button" className={s.btn}>Регистрация</button>
+                <button type="submit" className={s.btn}>Log in</button>
+                <button type="button" className={s.btn}>Sign up</button>
                     </div>
                     </div>
             </form>
