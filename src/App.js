710baddc41551297efc32b769bd4dff76c3bc233
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2022-02-05 17:33:51 +0200

    add adaptive header
    main and for loggedin users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Balance from './components/Balance/Balance';
 import RegistrationForm from '../src/components/RegistrationForm/RegistrationForm';
+import Header from './components/Header/Header';
 
 function App() {
-  
   return (
-     <>
+    <>
+      <Header />
       <Balance />
-      <RegistrationForm/>
-     </>
-    )
+      <RegistrationForm />
+    </>
+  );
 }
 
 export default App;
