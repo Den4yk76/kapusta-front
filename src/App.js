61b339833986e74ef0be0b950132d4507636086d
IrinaChoulgan <irina_choulgan@icloud.com>
2022-02-05 17:00:40 +0200

    сделала универсальную модалку

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Balance from './components/Balance/Balance';
 import RegistrationForm from '../src/components/RegistrationForm/RegistrationForm';
+import UniversalModal from './components/UniversalModal/UniversalModal';
 
 function App() {
-  
   return (
-     <>
+    <>
       <Balance />
-      <RegistrationForm/>
-     </>
-    )
+      <RegistrationForm />
+      <UniversalModal />
+    </>
+  );
 }
 
 export default App;
