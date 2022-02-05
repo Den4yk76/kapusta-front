29cb985b2a0e2a7c9a4f1bb4d84bb617ce1dca1e
IrinaChoulgan <irina_choulgan@icloud.com>
2022-02-05 17:02:22 +0200

    убрала импорт модалки в компоненте апп

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Balance from './components/Balance/Balance';
 import RegistrationForm from '../src/components/RegistrationForm/RegistrationForm';
-import UniversalModal from './components/UniversalModal/UniversalModal';
 
 function App() {
   return (
     <>
       <Balance />
       <RegistrationForm />
-      <UniversalModal />
     </>
   );
 }
