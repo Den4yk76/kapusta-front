7603bc1d6ec051e0f3fdb19c068b7f3fe2b193a7
Paul-Semenko <paul.semenko.ps@gmail.com>
2022-02-08 20:04:50 +0200

    sign up is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Balance from './components/Balance/Balance';
 import RegistrationForm from '../src/components/RegistrationForm/RegistrationForm';
 
 function App() {
   
   return (
-     <>
+    <>
+      
       <Balance />
-      <RegistrationForm/>
+        <RegistrationForm />
+        <ToastContainer autoClose={3000} />
      </>
     )
 }
