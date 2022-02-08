5d65ddea79d3ee954752a826179e4fb57949334f
Oleg-Khakhuda <buddy2317@gmail.com>
2022-02-08 22:33:33 +0200

    добавляет MobileList

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         {/*   <Hero /> */}
         <Balance />
         <RegistrationForm />
-        <Summary />
         <ExpenseIncome />
       </Container>
     </>
--- src/components/ExpenseIncome/TableExpense/TableExpense.js
+++ src/components/ExpenseIncome/TableExpense/TableExpense.js
@@ -1,8 +1,11 @@
+import Summary from "../../Summary/Summary";
+import { ReactComponent as Delete } from '../../../static/icons/delete.svg';
 import s from "../TableExpense/TableExpense.module.css";
 
 export default function TableExpense() {
     
     return (
+        <div className={s.container}>
         <div className={s.table__container}>
             <table className={s.table}>
                 <thead>
@@ -21,10 +24,18 @@ export default function TableExpense() {
                         <td>hahafha</td>
                         <td className={s.category}>hsghshs</td>
                         <td className={s.sumNegative}>hsghgsh</td>
-                        <td className={s.icon__bg}>hgfh</td>
+                        <td className={s.icon__bg}>
+                            <button className={s.icon__thumb}>
+                                <Delete className={s.icon__delete} />
+                            </button>
+                        </td>
                     </tr>
                 </tbody>
             </table>
+        </div>
+        <div>
+            <Summary />
+        </div>
         </div>
     )
 }
