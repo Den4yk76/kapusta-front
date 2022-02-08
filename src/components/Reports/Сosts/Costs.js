bfa0bf086bf50a0cccb42355796426acf2505d3a
IrinaChoulgan <irina_choulgan@icloud.com>
2022-02-08 18:48:53 +0200

    fix

--- src/components/Reports/Сosts/Costs.js
+++ src/components/Reports/Сosts/Costs.js
@@ -1,6 +1,5 @@
 import s from '../Сosts/Costs.module.css';
-// import { ReactComponent as Back } from '../../../static/icons/back.svg';
-// import { ReactComponent as Forward } from '../../../static/icons/forward.svg';
+import style from '../Income/Income.module.css';
 import { ReactComponent as Products } from '../../../static/icons/products.svg';
 import { ReactComponent as Cocktail } from '../../../static/icons/cocktail.svg';
 import { ReactComponent as Kite } from '../../../static/icons/kite.svg';
@@ -11,6 +10,8 @@ import { ReactComponent as Invoice } from '../../../static/icons/invoice.svg';
 import { ReactComponent as Clay } from '../../../static/icons/clay.svg';
 import { ReactComponent as Book } from '../../../static/icons/book.svg';
 import { ReactComponent as Ufo } from '../../../static/icons/ufo.svg';
+import { ReactComponent as Health } from '../../../static/icons/health.svg';
+import { ReactComponent as Oval } from '../../../static/icons/oval.svg';
 
 export default function Costs() {
   return (
@@ -18,62 +19,95 @@ export default function Costs() {
       <ul className={s.grid}>
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Products className={s.iconCategoriesActive} />
+          <div className={style.wrapperIcon}>
+            <Products className={`${style.classIcon}`} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>PRODUCTS</p>
         </li>
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
           <div className={s.wrap}>
-            <Cocktail />
+            <div className={style.wrapperIcon}>
+              <Cocktail className={s.iconCategories} />
+              <Oval className={`${style.classOval}`} />
+            </div>
           </div>
           <p className={s.titleIcons}>ALCOHOL</p>
         </li>
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Kite />
+          <div className={style.wrapperIcon}>
+            <Kite className={s.iconCategories} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>ENTERTAINMENT</p>
         </li>
 
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Products />
+          <div className={style.wrapperIcon}>
+            <Health className={s.iconCategories} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>HEALTH</p>
         </li>
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Car />
+          <div className={style.wrapperIcon}>
+            <Car className={s.iconCategories} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>TRANSPORT</p>
         </li>
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Couch />
+          <div className={style.wrapperIcon}>
+            <Couch className={s.iconCategories} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>HOUSING</p>
         </li>
 
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Tools />
+          <div className={style.wrapperIcon}>
+            <Tools className={s.iconCategories} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>TECHNICS</p>
         </li>
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Invoice />
+          <div className={style.wrapperIcon}>
+            <Invoice className={s.iconCategories} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>UTILITIES, CONNECTION</p>
         </li>
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Clay />
+          <div className={style.wrapperIcon}>
+            <Clay className={s.iconCategories} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>SPORT, HOBBY</p>
         </li>
 
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Book />
+          <div className={style.wrapperIcon}>
+            <Book className={s.iconCategories} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>EDUCATION</p>
         </li>
         <li className={s.gridItem}>
           <p className={s.titleSum}>5 000.00</p>
-          <Ufo />
+          <div className={style.wrapperIcon}>
+            <Ufo className={s.iconCategories} />
+            <Oval className={`${style.classOval}`} />
+          </div>
           <p className={s.titleIcons}>OTHER</p>
         </li>
       </ul>
