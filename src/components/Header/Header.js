bda447b760c02a5edc0ec0005e025a0495220271
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2022-02-09 23:56:41 +0200

    add link to index-page by tap on logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import logo from '../../static/logos/kapusta_logo.png';
-import s from './Header.module.css'
+import s from './Header.module.css';
 
 export default function Header() {
   return (
-      <header className={s.header}>
-          <img className={s.logo} width="90px" src={logo} alt="kapusta_logo" />
+    <header className={s.header}>
+      <a href="./">
+        <img className={s.logo} width="90px" src={logo} alt="kapusta_logo" />
+      </a>
     </header>
   );
 }
