5ffcbe4f253074b1cef76ca8b34d1db7b7c1ff84
IrinaChoulgan <irina_choulgan@icloud.com>
2022-02-09 18:06:16 +0200

    add validation for input-balance

--- src/components/LoginView/LoginView.js
+++ src/components/LoginView/LoginView.js
@@ -1,12 +1,8 @@
-import Header from '../Header/Header';
-import Hero from '../Hero/Hero';
 import RegistrationForm from '../RegistrationForm/RegistrationForm';
 
 export default function LoginView() {
   return (
     <>
-      {/* <Header /> */}
-      {/* <Hero /> */}
       <RegistrationForm />
     </>
   );
--- src/components/RegisterView/RegisterView.js
+++ src/components/RegisterView/RegisterView.js
@@ -1,12 +1,8 @@
-import Header from '../Header/Header';
-import Hero from '../Hero/Hero';
 import RegistrationForm from '../RegistrationForm/RegistrationForm';
 
 export default function RegisterView() {
   return (
     <>
-      {/* <Header /> */}
-      {/* <Hero /> */}
       <RegistrationForm />
     </>
   );
