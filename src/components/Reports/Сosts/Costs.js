18f02880bd13a52e3e73b822732f743c4b1a09a0
Nina <ninar313@gmail.com>
2022-02-15 14:01:34 +0200

    report data

--- src/components/Reports/Сosts/Costs.js
+++ src/components/Reports/Сosts/Costs.js
@@ -13,12 +13,31 @@ import { ReactComponent as Ufo } from '../../../static/icons/ufo.svg';
 import { ReactComponent as Health } from '../../../static/icons/health.svg';
 import { ReactComponent as Oval } from '../../../static/icons/oval.svg';
 
-export default function Costs() {
+export default function Costs({ data }) {
+  const getSum = category => {
+    const group = data.filter(el => el.category === category);
+    const sum = group.reduce((acc, el) => acc + Number(el.count), 0);
+    return sum;
+  };
+
   return (
     <>
       <ul className={s.grid}>
+        {/* {data.map(item => {
+          return (
+            <li className={s.gridItem} key={item.date}>
+              <p className={s.titleSum}>{item.count}</p>
+              <div className={style.wrapperIcon}>
+                <Products className={`${style.classIcon}`} />
+                <Oval className={`${style.classOval}`} />
+              </div>
+              <p className={s.titleIcons}>PRODUCTS</p>
+            </li>
+          );
+        })} */}
+
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('products')}</p>
           <div className={style.wrapperIcon}>
             <Products className={`${style.classIcon}`} />
             <Oval className={`${style.classOval}`} />
@@ -26,7 +45,7 @@ export default function Costs() {
           <p className={s.titleIcons}>PRODUCTS</p>
         </li>
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('alcohol')}</p>
           <div className={s.wrap}>
             <div className={style.wrapperIcon}>
               <Cocktail className={s.iconCategories} />
@@ -36,16 +55,15 @@ export default function Costs() {
           <p className={s.titleIcons}>ALCOHOL</p>
         </li>
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('entertainment')}</p>
           <div className={style.wrapperIcon}>
             <Kite className={s.iconCategories} />
             <Oval className={`${style.classOval}`} />
           </div>
           <p className={s.titleIcons}>ENTERTAINMENT</p>
         </li>
-
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('health')}</p>
           <div className={style.wrapperIcon}>
             <Health className={s.iconCategories} />
             <Oval className={`${style.classOval}`} />
@@ -53,7 +71,7 @@ export default function Costs() {
           <p className={s.titleIcons}>HEALTH</p>
         </li>
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('transport')}</p>
           <div className={style.wrapperIcon}>
             <Car className={s.iconCategories} />
             <Oval className={`${style.classOval}`} />
@@ -61,16 +79,15 @@ export default function Costs() {
           <p className={s.titleIcons}>TRANSPORT</p>
         </li>
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('housing')}</p>
           <div className={style.wrapperIcon}>
             <Couch className={s.iconCategories} />
             <Oval className={`${style.classOval}`} />
           </div>
           <p className={s.titleIcons}>HOUSING</p>
         </li>
-
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('technics')}</p>
           <div className={style.wrapperIcon}>
             <Tools className={s.iconCategories} />
             <Oval className={`${style.classOval}`} />
@@ -78,7 +95,7 @@ export default function Costs() {
           <p className={s.titleIcons}>TECHNICS</p>
         </li>
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('utilityCommunication')}</p>
           <div className={style.wrapperIcon}>
             <Invoice className={s.iconCategories} />
             <Oval className={`${style.classOval}`} />
@@ -86,16 +103,15 @@ export default function Costs() {
           <p className={s.titleIcons}>UTILITIES, CONNECTION</p>
         </li>
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('sportsHobbies')}</p>
           <div className={style.wrapperIcon}>
             <Clay className={s.iconCategories} />
             <Oval className={`${style.classOval}`} />
           </div>
           <p className={s.titleIcons}>SPORT, HOBBY</p>
         </li>
-
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('education')}</p>
           <div className={style.wrapperIcon}>
             <Book className={s.iconCategories} />
             <Oval className={`${style.classOval}`} />
@@ -103,7 +119,7 @@ export default function Costs() {
           <p className={s.titleIcons}>EDUCATION</p>
         </li>
         <li className={s.gridItem}>
-          <p className={s.titleSum}>5 000.00</p>
+          <p className={s.titleSum}>{getSum('other')}</p>
           <div className={style.wrapperIcon}>
             <Ufo className={s.iconCategories} />
             <Oval className={`${style.classOval}`} />
